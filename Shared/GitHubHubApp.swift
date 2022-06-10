28c64e4121163fd9e8a5674778a62c5721a2cb0e
Daniel Krofchick <krofchick@gmail.com>
2022-06-09 22:42:19 -0400

    Initial Commit

---
+++ Shared/GitHubHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubHubApp.swift
+//  Shared
+//
+//  Created by Daniel Krofchick on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct GitHubHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
