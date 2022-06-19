8a188699de9843b36d9c89171fc7be1d7fa0e6d2
Daniel Krofchick <krofchick@gmail.com>
2022-06-19 00:06:34 -0400

    Refactor

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,29 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var user: UserView?
-
     var body: some View {
-        user
-        Rectangle()
-            .frame(width: 0, height: 0, alignment: .center)
-            .onAppear {
-                loadData()
-            }
-            .hidden()
-    }
-
-    private func loadData() {
-        Task {
-            do {
-                let user = try await GitHub().user("DanielKrofchick")
-                if let fragment = user.data?.user?.fragments.userFragment {
-                    self.user = UserView(fragment)
-                }
-            } catch {
-                print(error)
-            }
-        }
+        UserView("DanielKrofchick")
     }
 }
 
--- Shared/UserView.swift
+++ Shared/UserView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct UserView: View {
-    let name: String?
-    let avatarURL: URL?
+    @State private var name: String?
+    @State private var avatarURL: URL?
+    private let login: String
+
+    init(_ login: String) {
+        self.login = login
+    }
 
     var body: some View {
         VStack {
@@ -30,23 +35,34 @@ struct UserView: View {
                     Circle().stroke(.gray, lineWidth: 4)
                 }
                 .shadow(radius: 7)
-            Text(name ?? "")
+            Text(name ?? " ")
         }
+        .onAppear {
+            loadData()
+        }
+    }
+
+    private func loadData() {
+        Task {
+            do {
+                let user = try await GitHub().user(login)
+                if let fragment = user.data?.user?.fragments.userFragment {
+                    bind(fragment)
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private func bind(_ fragment: UserFragment) {
+        name = fragment.name
+        avatarURL = URL(string: fragment.avatarUrl)
     }
 }
 
 struct UserView_Previews: PreviewProvider {
     static var previews: some View {
-        UserView(
-            name: "Boba Fett",
-            avatarURL: nil
-        )
-    }
-}
-
-extension UserView {
-    init(_ data: UserFragment) {
-        name = data.name
-        avatarURL = URL(string: data.avatarUrl)
+        UserView("DanielKrofchick")
     }
 }
